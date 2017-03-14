207d553b159ea6c8360b21f8e0901725860533d5
Felix Chapman <aelred717@gmail.com>
2017-03-14 22:30:44 +0000

    Add tests for `pos.rs`

--- src/pos.rs
+++ src/pos.rs
@@ -1,6 +1,6 @@
 use std::ops::Add;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Pos {
     pub x: isize,
     pub y: isize,
@@ -13,6 +13,37 @@ impl Add for Pos {
         Pos {
             x: self.x + other.x,
             y: self.y + other.y,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck::{Arbitrary, Gen};
+
+    impl Arbitrary for Pos {
+        fn arbitrary<G: Gen>(g: &mut G) -> Pos {
+            // We don't need to worry about positions that might overflow
+            Pos {
+                x: g.gen_range(-30, 30),
+                y: g.gen_range(-30, 30),
+            }
+        }
+    }
+
+    quickcheck! {
+        fn add_is_commutative(a: Pos, b: Pos) -> bool {
+            a + b == b + a
+        }
+
+        fn add_is_associative(a: Pos, b: Pos, c: Pos) -> bool {
+            (a + b) + c == a + (b + c)
+        }
+
+        fn add_has_identity_element(p: Pos) -> bool {
+            let id = Pos {x: 0, y: 0};
+            p + id == p && id + p == p
         }
     }
 }
