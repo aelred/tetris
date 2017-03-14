e217ac96d33d22f52667b381cb96b31d5af439e6
Felix Chapman <aelred717@gmail.com>
2017-03-14 22:30:51 +0000

    Add tests for `board.rs`

--- src/board.rs
+++ src/board.rs
@@ -7,6 +7,7 @@ use sdl2::render::Renderer;
 pub const WIDTH: usize = 10;
 pub const HEIGHT: usize = 24;
 
+#[derive(Clone, Debug)]
 pub struct Board([[Option<Color>; WIDTH]; HEIGHT]);
 
 impl Board {
@@ -15,9 +16,7 @@ impl Board {
     }
 
     pub fn touches(&self, pos: Pos) -> bool {
-        pos.y >= 0 &&
-        (pos.x < 0 || pos.x >= WIDTH as isize || pos.y >= HEIGHT as isize ||
-         self.0[pos.y as usize][pos.x as usize].is_some())
+        pos.y >= 0 && (out_bounds(pos) || self.0[pos.y as usize][pos.x as usize].is_some())
     }
 
     pub fn fill(&mut self, pos: Pos, color: Color) {
@@ -62,6 +61,76 @@ impl Board {
                     None => (),
                 }
             }
+        }
+    }
+}
+
+fn out_bounds(pos: Pos) -> bool {
+    pos.x < 0 || pos.x >= WIDTH as isize || pos.y >= HEIGHT as isize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck::{Arbitrary, Gen, TestResult};
+    use std::mem;
+    use std::ptr;
+    use sdl2::pixels::Color;
+
+    impl Arbitrary for Board {
+        fn arbitrary<G: Gen>(g: &mut G) -> Board {
+            unsafe {
+                let mut array: [[Option<Color>; WIDTH]; HEIGHT] = mem::uninitialized();
+
+                for row in array.iter_mut() {
+                    for cell in row.iter_mut() {
+                        let color = if g.gen() {
+                            Some(Color::RGB(g.gen(), g.gen(), g.gen()))
+                        } else {
+                            None
+                        };
+
+                        ptr::write(cell, color);
+                    }
+                }
+
+                Board(array)
+            }
+        }
+    }
+
+    quickcheck! {
+
+        fn a_new_board_is_empty(pos: Pos) -> TestResult {
+            if out_bounds(pos) || pos.y < 0 {
+                return TestResult::discard();
+            }
+            TestResult::from_bool(!Board::new().touches(pos))
+        }
+
+        fn after_filling_a_space_it_is_filled(board: Board, pos: Pos, r: u8, g: u8, b: u8) -> TestResult {
+            if out_bounds(pos) || pos.y < 0 {
+                return TestResult::discard();
+            }
+
+            let color = Color::RGB(r, g, b);
+            let mut board = board;
+            board.fill(pos, color);
+            TestResult::from_bool(board.touches(pos))
+        }
+
+        fn after_filling_a_space_no_other_space_changes(board: Board, pos1: Pos, pos2: Pos, r: u8, g: u8, b: u8) -> TestResult {
+            if pos1 == pos2 || out_bounds(pos1) || pos1.y < 0 {
+                return TestResult::discard();
+            }
+
+            let color = Color::RGB(r, g, b);
+            let mut board = board;
+
+            let touches_before = board.touches(pos2);
+            board.fill(pos1, color);
+            let touches_after = board.touches(pos2);
+            TestResult::from_bool(touches_before == touches_after)
         }
     }
 }
