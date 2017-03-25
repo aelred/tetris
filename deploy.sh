7c50c00844130d824917e2481484e29499e95271
Felix Chapman <aelred717@gmail.com>
2017-03-25 23:59:39 +0000

    Change travis deploy to happen server side

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,12 @@
 set -e
 set -o pipefail
 
+cd ~/sites/tetris
+
+source ~/scratch/emsdk-portable/emsdk_env.sh
+
+git pull
+
 cargo build --target asmjs-unknown-emscripten --release
 
 cp target/asmjs-unknown-emscripten/release/tetris.js static/tetris.js
-
-scp -r static felix@ael.red:~/sites/tetris/
