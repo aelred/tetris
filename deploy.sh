5578fd4676427d84dbca7de6c3d2af56ce2aef43
Felix Chapman <aelred717@gmail.com>
2017-03-26 08:47:12 +0100

    Revert previous commit

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ source ~/scratch/emsdk-portable/emsdk_env.sh
 
 git pull
 
-cargo clean
 cargo build --target asmjs-unknown-emscripten --release
 
 cp target/asmjs-unknown-emscripten/release/tetris.js static/tetris.js
