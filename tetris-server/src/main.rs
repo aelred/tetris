83c55d55948e70348b0b631728ba4f5b53582e36
Felix Chapman <aelred717@gmail.com>
2017-04-06 21:57:24 +0100

    Add support for POSTing hiscores

--- tetris-server/src/main.rs
+++ tetris-server/src/main.rs
@@ -6,10 +6,15 @@ use lib::score::Score;
 
 use rustc_serialize::json;
 
-use hyper::Get;
-use hyper::server::{Server, Request, Response};
+use std::error::Error;
+use std::sync::RwLock;
+use std::io::Read;
+
+use hyper::{Get, Post};
+use hyper::server::{Server, Handler, Request, Response};
 use hyper::uri::RequestUri::AbsolutePath;
 use hyper::header::ContentType;
+use hyper::status::StatusCode;
 
 macro_rules! try_return(
     ($e:expr) => {{
@@ -20,40 +25,90 @@ macro_rules! try_return(
     }}
 );
 
-fn scores(req: Request, mut res: Response) {
-    match req.uri {
-        AbsolutePath(ref path) => {
+macro_rules! print_err(
+    ($e:expr) => {{
+        if let Err(e) = $e {
+            println!("{}", e);
+        }
+    }}
+);
+
+struct ScoresHandler {
+    hiscores: RwLock<Vec<Score>>,
+}
+
+impl ScoresHandler {
+    fn add_hiscore(&self, req: &mut Request, mut res: Response) -> Result<(), Box<Error>> {
+        let mut body = String::new();
+        try!(req.read_to_string(&mut body));
+
+        let score: Score = match json::decode(&body) {
+            Ok(s) => s,
+            Err(_) => {
+                *res.status_mut() = StatusCode::BadRequest;
+                return Ok(());
+            }
+        };
+
+        if !score.name.chars().all(char::is_alphanumeric) || score.name.chars().count() > 3 {
+            *res.status_mut() = StatusCode::BadRequest;
+            return Ok(());
+        }
+
+        {
+            let mut hiscores = self.hiscores.write().unwrap();
+            hiscores.push(score);
+            hiscores.sort_by_key(|s| std::u32::MAX - s.value);
+            hiscores.pop();
+        }
+
+        *res.status_mut() = StatusCode::Created;
+
+        try!(self.send_hiscores(res));
+        Ok(())
+    }
+
+    fn send_hiscores(&self, mut res: Response) -> std::io::Result<()> {
+        let ref hiscores = *self.hiscores.read().unwrap();
+
+        res.headers_mut().set(ContentType::json());
+        let body = json::encode(hiscores).unwrap();
+        res.send(body.as_bytes())
+    }
+}
+
+impl Handler for ScoresHandler {
+    fn handle(&self, mut req: Request, mut res: Response) {
+        if let AbsolutePath(path) = req.uri.clone() {
             match (&req.method, &path[..]) {
                 (&Get, "/scores") => {
-                    res.headers_mut().set(ContentType::json());
-                    let scores = get_hiscores();
-                    try_return!(res.send(json::encode(&scores).unwrap().as_bytes()));
+                    print_err!(self.send_hiscores(res));
+                }
+                (&Post, "/scores") => {
+                    print_err!(self.add_hiscore(&mut req, res));
                 }
                 _ => {
                     *res.status_mut() = hyper::NotFound;
                 }
             }
-        }
-        _ => {}
-    };
+        };
 
-    return;
+        return;
+    }
 }
 
 fn main() {
     let server = Server::http("localhost:4444").unwrap();
-    let _ = server.handle(scores);
+
+    let handler = ScoresHandler { hiscores: RwLock::new(init_hiscores()) };
+
+    server.handle(handler).unwrap();
 }
 
-fn get_hiscores() -> Vec<Score> {
-    vec![Score::new(1000, "FEL".to_string()),
-         Score::new(900, "ANG".to_string()),
-         Score::new(800, "LLY".to_string()),
-         Score::new(700, "MKO".to_string()),
-         Score::new(600, "ALX".to_string()),
-         Score::new(500, "JSN".to_string()),
-         Score::new(400, "SHD".to_string()),
-         Score::new(300, "CHR".to_string()),
-         Score::new(200, "SRH".to_string()),
-         Score::new(100, "EMY".to_string())]
+fn init_hiscores() -> Vec<Score> {
+    let mut hiscores = Vec::new();
+    for _ in 0..10 {
+        hiscores.push(Score::new(0, "AEL".to_string()));
+    }
+    hiscores
 }
