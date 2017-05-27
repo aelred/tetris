2d135ef5387091298d1664f7f80de58f6e26b491
Felix Chapman <aelred717@gmail.com>
2017-05-27 11:50:03 +0100

    Make emscripten linker always use right environment

--- deploy/js.sh
+++ deploy/js.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /opt/emsdk-portable/emsdk_env.sh
 cargo build --bin tetris --target asmjs-unknown-emscripten --release
 cp target/asmjs-unknown-emscripten/release/tetris.js static/tetris.js
 
