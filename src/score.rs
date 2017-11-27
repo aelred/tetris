93b52e386e7dce21e95d6e121c48b6496bccb565
Felix Chapman <aelred717@gmail.com>
2017-11-27 12:50:18 +0000

    Add test for bug

--- src/score.rs
+++ src/score.rs
@@ -106,12 +106,23 @@ mod tests {
     use serde_json;
 
     #[test]
-    fn correctly_recognise_a_valid_game() {
-        let body = include_str!("../resources/valid-game.json");
+    fn correctly_recognise_a_valid_short_game() {
+        let body = include_str!("../resources/games/short.json");
         let message: ScoreMessage = serde_json::from_str(&body).unwrap();
         assert_eq!(
             message.score().unwrap(),
             Score::new(1700, "AEL".to_string())
+        );
+    }
+
+    #[test]
+    #[ignore] // TODO: fix whatever causes this to fail
+    fn correctly_recognise_a_valid_long_game() {
+        let body = include_str!("../resources/games/long.json");
+        let message: ScoreMessage = serde_json::from_str(&body).unwrap();
+        assert_eq!(
+            message.score().unwrap(),
+            Score::new(24800, "AEL".to_string())
         );
     }
 }
