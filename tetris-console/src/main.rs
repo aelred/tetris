804ebe94f1dea8fdaf6cd608ef30b5a0f40a27ff
Felix Chapman <aelred717@gmail.com>
2018-11-26 18:56:39 +0000

    Draw a border and other game states in console

--- tetris-console/src/main.rs
+++ tetris-console/src/main.rs
@@ -6,11 +6,11 @@ use std::io::Write;
 use termion::color;
 use termion::color::Color;
 use termion::color::Rgb;
+use termion::cursor;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use tetris::shape::ShapeColor;
 use tetris::state::State;
-use termion::cursor;
 
 fn main() {
     let stdout = io::stdout();
@@ -23,19 +23,19 @@ fn main() {
 
     loop {
         match &state {
-            State::Title(title) => {
-                write!(stdout, "{}{}", cursor::Goto(1, 1), "TETRIS: Press Enter");
+            State::Title(_) => {
+                write!(stdout, "{}TETRIS: Press Enter", cursor::Goto(1, 1)).unwrap();
             }
             State::Play(ref game) => {
                 draw_game(&mut stdout, &game.game);
             }
-            State::Paused(paused) => {
+            State::Paused(_) => {
                 // TODO
-                write!(stdout, "{}{}", cursor::Goto(1, 1), "PAUSED");
+                write!(stdout, "{}PAUSED", cursor::Goto(1, 1)).unwrap();
             }
-            State::GameOver(game_over) => {
+            State::GameOver(_) => {
                 // TODO
-                write!(stdout, "{}{}", cursor::Goto(1, 1), "Not implemented");
+                write!(stdout, "{}Not implemented", cursor::Goto(1, 1)).unwrap();
             }
         }
 
@@ -62,34 +62,30 @@ fn main() {
                     Key::Char('\n') => title.start_game(),
                     _ => State::from(title),
                 },
-                State::Play(mut game) => {
-                    match b {
-                        Key::Up => {
-                            game.rotate();
-                            State::from(game)
-                        },
-                        Key::Left => {
-                            game.move_left();
-                            State::from(game)
-                        },
-                        Key::Right => {
-                            game.move_right();
-                            State::from(game)
-                        },
-                        Key::Down => {
-                            game.start_soft_drop();
-                            State::from(game)
-                        },
-                        Key::Char(' ') => {
-                            game.start_hard_drop();
-                            State::from(game)
-                        },
-                        Key::Char('\n') => game.pause(),
-                        _ => {
-                            State::from(game)
-                        }
+                State::Play(mut game) => match b {
+                    Key::Up => {
+                        game.rotate();
+                        State::from(game)
                     }
-                }
+                    Key::Left => {
+                        game.move_left();
+                        State::from(game)
+                    }
+                    Key::Right => {
+                        game.move_right();
+                        State::from(game)
+                    }
+                    Key::Down => {
+                        game.start_soft_drop();
+                        State::from(game)
+                    }
+                    Key::Char(' ') => {
+                        game.start_hard_drop();
+                        State::from(game)
+                    }
+                    Key::Char('\n') => game.pause(),
+                    _ => State::from(game),
+                },
                 State::Paused(paused) => paused.unpause(),
                 State::GameOver(game_over) => State::from(game_over),
             };
@@ -103,15 +99,23 @@ const BLOCK_WIDTH: u16 = 2;
 const SPACE: &str = "  ";
 const BLOCK: &str = "▐▉";
 
+const HOR_BORDER: &str = "══";
+const VERT_BORDER: &str = "║";
+const TL_BORDER: &str = "╔";
+const BL_BORDER: &str = "╚";
+const TR_BORDER: &str = "╗";
+const BR_BORDER: &str = "╝";
+
 fn draw_game<W: Write>(stdout: &mut W, game: &tetris::game::Game) {
     let mut buffer = std::io::BufWriter::new(stdout);
+    draw_border(&mut buffer);
     draw_board(&mut buffer, &game.board);
     draw_piece(&mut buffer, &game.piece);
 }
 
 fn draw_board<W: Write>(stdout: &mut W, board: &tetris::board::Board) {
     for (num, row) in board.grid.iter().enumerate() {
-        write!(stdout, "{}", termion::cursor::Goto(1, num as u16 + 1)).unwrap();
+        write!(stdout, "{}", termion::cursor::Goto(2, num as u16 + 2)).unwrap();
 
         for cell in row.iter() {
             match cell {
@@ -131,13 +135,43 @@ fn draw_board<W: Write>(stdout: &mut W, board: &tetris::board::Board) {
     }
 }
 
+fn draw_border<W: Write>(stdout: &mut W) {
+    write!(stdout, "{}", color::Fg(color::White)).unwrap();
+    write!(
+        stdout,
+        "{}{}{}{}",
+        termion::cursor::Goto(1, 1),
+        TL_BORDER,
+        HOR_BORDER.repeat(tetris::board::WIDTH as usize),
+        TR_BORDER
+    ).unwrap();
+    for row in 0..u16::from(tetris::board::HEIGHT) {
+        write!(
+            stdout,
+            "{}{}{}{}",
+            termion::cursor::Goto(1, row + 2),
+            VERT_BORDER,
+            termion::cursor::Goto((u16::from(tetris::board::WIDTH) * BLOCK_WIDTH) + 2, row + 2),
+            VERT_BORDER
+        ).unwrap();
+    }
+    write!(
+        stdout,
+        "{}{}{}{}",
+        termion::cursor::Goto(1, u16::from(tetris::board::HEIGHT) + 2),
+        BL_BORDER,
+        HOR_BORDER.repeat(tetris::board::WIDTH as usize),
+        BR_BORDER
+    ).unwrap();
+}
+
 fn draw_piece<W: Write>(stdout: &mut W, piece: &tetris::piece::Piece) {
     let color = shape_color_to_ascii_color(piece.shape.color);
     write!(stdout, "{}", color::Fg(color)).unwrap();
 
     for pos in piece.blocks().iter() {
-        let cursor_x = (pos.x() as u16) * BLOCK_WIDTH + 1;
-        let cursor_y = pos.y() as u16 + 1;
+        let cursor_x = (pos.x() as u16) * BLOCK_WIDTH + 2;
+        let cursor_y = pos.y() as u16 + 2;
         let cursor = termion::cursor::Goto(cursor_x, cursor_y);
         write!(stdout, "{}{}", cursor, BLOCK).unwrap();
     }
