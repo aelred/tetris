8556a0b5157e5ddf851ff107ff3a4e732ea55ff4
Felix Chapman <aelred717@gmail.com>
2018-11-30 00:32:30 +0000

    Add bash magic

--- deploy/js.sh
+++ deploy/js.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# fail immediately if a command fails
+set -e
+set -o pipefail
+
 cd tetris-sdl
 cargo build --release --target asmjs-unknown-emscripten
 cd ..
--- deploy/server.sh
+++ deploy/server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# fail immediately if a command fails
+set -e
+set -o pipefail
+
 cd tetris-server
 cargo install --force
 sudo systemctl restart tetris
