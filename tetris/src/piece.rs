bab3ba6574be7690e0cb7dd12f9d3aa751f4cc3c
Felix Chapman <aelred717@gmail.com>
2018-12-05 21:03:07 +0000

    Document the piece module

--- tetris/src/piece.rs
+++ tetris/src/piece.rs
@@ -3,16 +3,24 @@ use pos::Pos;
 use shape::Rotation;
 use shape::Shape;
 
+/// The initial X position that a piece spawns on the board.
 const INITIAL_X: i16 = Board::WIDTH as i16 / 2 - 2;
 
+/// A tetromino piece in play.
 #[derive(Debug)]
 pub struct Piece {
+    /// The shape of the piece.
     pub shape: Shape,
+
+    /// The orientation of the piece.
     pub rot: Rotation,
+
+    /// The position of the piece on the board.
     pub pos: Pos,
 }
 
 impl Piece {
+    /// Create a new piece at the initial position and rotation.
     pub fn new(shape: Shape) -> Piece {
         Piece {
             shape,
@@ -21,30 +29,37 @@ impl Piece {
         }
     }
 
+    /// Rotate the piece clockwise.
     pub fn rotate_clockwise(&mut self) {
         self.rot = self.rot.clockwise();
     }
 
+    /// Rotate the piece anticlockwise.
     pub fn rotate_anticlockwise(&mut self) {
         self.rot = self.rot.anticlockwise();
     }
 
+    /// Move the piece one space left.
     pub fn left(&mut self) {
         self.pos = self.pos.left();
     }
 
+    /// Move the piece one space right.
     pub fn right(&mut self) {
         self.pos = self.pos.right();
     }
 
+    /// Move the piece one space up.
     pub fn up(&mut self) {
         self.pos = self.pos.up();
     }
 
+    /// Move the piece one space down.
     pub fn down(&mut self) {
         self.pos = self.pos.down();
     }
 
+    /// Get all the blocks that make up the piece at the current position and rotation.
     pub fn blocks(&self) -> Vec<Pos> {
         self.shape
             .blocks(self.rot)
@@ -55,5 +70,6 @@ impl Piece {
 }
 
 lazy_static! {
+    /// The initial position that a piece spawns on the board.
     static ref INITIAL_POS: Pos = Pos::new(INITIAL_X, 0);
 }
