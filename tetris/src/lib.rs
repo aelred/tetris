5a66be554d3f65ae7f05cfb934892de0b0bf89dc
Felix Chapman <aelred717@gmail.com>
2018-12-19 11:33:16 +0000

    Add example to docs

--- tetris/src/lib.rs
+++ tetris/src/lib.rs
@@ -1,3 +1,44 @@
+//! Library representing a Tetris game.
+//! ```
+//! use tetris::State;
+//! use tetris::State::*;
+//!
+//! // Initialise a game at the title screen
+//! let mut state = State::title();
+//!
+//! loop {
+//!     // Match on the state of the game and get a new state back.
+//!     state = match state {
+//!         Title(title) => {
+//!             title.start_game()
+//!         },
+//!         Play(mut game) => {
+//!             // Play some random moves
+//!             game.move_left();
+//!             game.rotate();
+//!             game.start_hard_drop();
+//!             game.into()  // `.into()` transforms the inner states back into the `State` enum
+//!         }
+//!         Paused(paused) => {
+//!             paused.unpause()
+//!         }
+//!         GameOver(mut game_over) => {
+//!             // Submit my high-score!
+//!             game_over.push_name("BOB");
+//!             game_over.submit()
+//!         }
+//!     };
+//!
+//!     // Update the state of the game by one tick
+//!     state = state.update();
+//!
+//!     // Draw game state and sleep
+//!     // ...
+//!
+//!     # break;
+//! }
+//! ```
+
 pub use self::board::Board;
 pub use self::game::{Game, GameWithHistory};
 pub use self::game_over::{GameOver, HighScores};
