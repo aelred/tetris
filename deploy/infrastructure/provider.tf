70d4686f38a7dfe42b6f7cf96bcec2cce646cd96
Felix Chapman <aelred717@gmail.com>
2022-01-10 22:24:57 +0000

    remove required_providers block

--- deploy/infrastructure/provider.tf
+++ deploy/infrastructure/provider.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-    github = {
-      source = "integrations/github"
-      version = "~> 4.4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0.2"
-    }
-  }
-}
-
 provider aws {
   region = "eu-west-2"
 }
